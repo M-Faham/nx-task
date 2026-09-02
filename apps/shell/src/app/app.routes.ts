077905b605e85dbc7efb4b14c678b5da72191f6d
Faham <m.faham3@gmail.com>
2026-09-02 23:40:24 +0300

    add routing for the remotes

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'table',
+    loadComponent: () =>
+      loadRemoteModule('table', './Component').then((m) => m.App),
+  },
+  {
+    path: 'data',
+    loadComponent: () =>
+      loadRemoteModule('data', './Component').then((m) => m.App),
+  },
+];
